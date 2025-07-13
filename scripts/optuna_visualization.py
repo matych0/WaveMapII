5b335d1826ccb1eda3196838dfe7ebd0161a7acb
Martin Mátych <martinmatych@gmail.com>
2025-07-13 13:45:17 +0200

    Optuna results visualization

--- scripts/optuna_visualization.py
+++ scripts/optuna_visualization.py
@@ -3,6 +3,15 @@ import optuna
 from optuna.visualization import matplotlib as vis_matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import optuna.visualization as vis
+import pandas as pd
+import plotly.io as pio
+import plotly.graph_objects as go
+from plotly.io import show
+import plotly.express as px
+
+
+#pio.renderers.default = "browser"
 #%%
 study = optuna.load_study(study_name='hyperparam_optim_mult_controls_CUDA', storage='sqlite:///D:/logs/logs_160_runs/hyperparam_optim_mult_controls_CUDA')
 
@@ -46,66 +55,93 @@ def plot_optuna_history(study):
 
     plt.tight_layout()
     plt.show()
-    
-#%%
-optuna.visualization.plot_contour(study, params=['learning_rate', 'batch_size', 'n_controls'], target_name="Concordance Index")
+
 
 #%%
+def plot_optuna_contour(study, params=['learning_rate', 'batch_size', 'n_controls'], target_name="Concordance Index"):
+    fig = optuna.visualization.plot_contour(study, params=params, target_name=target_name)
 
-log_params = [0,2]
+    pio.write_image(fig, "C:/Users/marti/Documents/Diplomka/images/optuna_contour_plot_training.pdf",  width=600, height=600)
 
-fig = vis_matplotlib.plot_contour(study, params=['cox_regularization', 'weight_decay', 'dropout'], target_name="Concordance Index")
+    show(fig)
 
-fig = plt.gcf()
-ax = np.array(fig.axes)[:-1].reshape(3, 3)
 
-fig.suptitle("")
+#%%
+def plot_optuna_param_importances(study):
+    ax = vis_matplotlib.plot_param_importances(study, target_name="Concordance Index")
+    fig = ax.figure
 
-ax[0][0].set_facecolor('white') 
-ax[1][1].set_facecolor('white')
-ax[2][2].set_facecolor('white')
+    ax.get_legend().remove()
+    ax.set_title("")
+    # Background color
+    ax.set_facecolor('white')               # Plot background
+    fig.patch.set_facecolor('white')
 
-#ax.set_title("")
-
-for i, ax_row in enumerate(ax):
-    for j, _ in enumerate(ax_row):
-        ax[i][j].set_aspect('auto')
+    ax.set_title('')
+    ax.tick_params(axis='both', colors='black', labelsize=10)
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_color('black')
+        label.set_fontsize(10)
         
-        #ax_col.tick_params(axis='both', colors='black', labelsize=10)
-        if i < len(ax) - 1:
-            ax[i][j].set_xlabel('')
-            ax[i][j].set_xticks([])
-        if j > 0:
-            ax[i][j].set_ylabel('')
-            ax[i][j].set_yticks([])
-            
-        if i in log_params:
-            if j in log_params:
-                ax[i][j].set_xlim(left=1e-4, right=1e-1)
-                ax[i][j].set_xscale('log')
-            
-                ax[i][j].set_ylim(bottom=1e-4, top=1e-1)
-                ax[i][j].set_yscale('log')
-        """ ax_col.set_xlabel(ax_col.get_xlabel(), fontsize=10, color='black')
-        ax_col.set_ylabel(ax_col.get_ylabel(), fontsize=10, color='black')
-        for label in ax_col.get_xticklabels() + ax_col.get_yticklabels():
-            label.set_color('black')
-            label.set_fontsize(10) """
-""" ax.set_xscale('log')
-ax.set_xlabel('Log Param1', fontsize=12, color='black')
-ax.set_ylabel('Param2', fontsize=12, color='black')
-ax.tick_params(axis='both', colors='black', labelsize=10)
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False) """
-
-""" colorbar_ax = fig.axes[-1]
-colorbar_ax.set_facecolor('white')
-colorbar_ax.tick_params(colors='black') """
-#%%
+    ax.set_xlabel('Hyperparameter Importance', fontsize=11, color='black')
+    ax.set_ylabel('Hyperparameter', fontsize=11, color='black')
+        
+    ax.grid(True, linestyle='--', alpha=0.6)
+    for spine in ax.spines.values():
+        spine.set_edgecolor('black')
+        
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
     
+    
+    ax.figure.tight_layout()
+    
+    plt.show()
 
 
+#%%
+#vis_matplotlib.plot_slice(study, params=["num_epochs"], target_name="Concordance Index")
+
+def plot_optuna_slice(study):
+    ax = vis_matplotlib.plot_slice(study, params=["num_epochs"], target_name="Concordance Index")
+    fig = ax.figure
+    
+    ax.set_title("")
+    # Background color
+    ax.set_facecolor('white')               # Plot background
+    fig.patch.set_facecolor('white')
+
+    ax.set_title('')
+    ax.tick_params(axis='both', colors='black', labelsize=10)
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_color('black')
+        label.set_fontsize(10)
+
+    ax.set_xlabel('Epochs', fontsize=11, color='black')
+    ax.set_ylabel('Concordance Index', fontsize=11, color='black')
+        
+    ax.grid(True, linestyle='--', alpha=0.6)
+    for spine in ax.spines.values():
+        spine.set_edgecolor('black')
+        
+    ax.set_ylim(0.5, 0.65)  # Adjust y-axis limits as needed
+    ax.set_xscale('log')
+    ax.set_xticks([50,60,70,80,90, 100, 200, 300, 400, 500])
+    ax.set_xticklabels([50,60,70,80,90, 100, 200, 300, 400, 500])  # Set x-ticks from 50 to 500 with a step of 10
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    
+    
+    ax.figure.tight_layout()
+    
+    plt.show()
+
+#%%
 
     
 if __name__ == "__main__":
-    plot_optuna_history(study)
+    #plot_optuna_history(study)
+    %matplotlib qt
+    #plot_optuna_param_importances(study)
+    plot_optuna_slice(study)
